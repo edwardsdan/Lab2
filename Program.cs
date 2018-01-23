a56efe3267285d5f448e1dfef7166e03ceff29ba
DanE <edwardsdan001@gmail.com>
2018-01-23 14:51:35 -0500

    polished and reorganized

--- Program.cs
+++ Program.cs
@@ -16,33 +16,33 @@ namespace Lab2
             Console.WriteLine($"Hello, {Name}! Please enter a number between 1 and 100");
             int Input = int.Parse(Console.ReadLine());
 
-            if (Input == 1 || Input == 100 || Input < 1 || Input > 100)
+            if (Input <= 1 || Input >= 100)
             {
                 Console.WriteLine("You chose a number outside the range! Try again!");
                 Main(args);
             }
             else
             {
-                if (Input % 2 != 0)
+                if (Input % 2 == 0)
+                {
+                    if (Input >= 2 && Input <= 25)
+                    {
+                        Console.WriteLine($"{Name}'s number ({Input}) is less than 25 and even");
+                    }
+                    else if (Input >= 26 && Input <= 60)
+                    {
+                        Console.WriteLine($"{Name}'s number is even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{Name}'s number ({Input}) is even");
+                    }
+                }
+                else
                 {
                     Console.WriteLine($"{Name}'s number ({Input}) is odd");
                 }
-                else if (Input % 2 == 0 && Input >= 2 && Input <= 25)
-                {
-                    Console.WriteLine($"{Name}'s number ({Input}) is less than 25 and even");
-                }
-                else if (Input % 2 == 0 && Input >= 26 && Input <= 60)
-                {
-                    Console.WriteLine($"{Name}'s number is even");
-                }
-                else if (Input % 2 == 0 && Input > 60)
-                {
-                    Console.WriteLine($"{Name}'s number ({Input}) is even");
-                }
-                else if (Input % 2 != 0 && Input > 60)
-                {
-                    Console.WriteLine($"{Name}'s number ({Input}) is odd");
-                }
+                
                 Console.WriteLine("Would you like to go again? (Y/N)");
                 string response = Console.ReadLine();
                 if (response == "y" || response == "Y")
